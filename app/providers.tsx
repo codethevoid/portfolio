e89b4a7b5004732e847164120beb8485e147cbb4
rmthomas <rmthomas1998@gmail.com>
2025-07-05 10:37:45 -0400

    redesign

---
+++ app/providers.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "next-themes";
+import { ReactNode } from "react";
+
+export const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
